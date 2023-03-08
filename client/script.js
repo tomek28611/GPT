e6b9c78cebeb212bdc56c190f90b5edb511e0640
Tomek28611 <tomek286@yahoo.com>
2023-03-08 08:10:37 +0100

    commit2

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('', {
+    const response = await fetch('https://gpt-or7y.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
